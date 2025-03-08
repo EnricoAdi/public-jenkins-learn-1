6e91a6fb250368cb8a5164bd78353341fa0e1a8f
Enrico <enricoadi49@gmail.com>
2025-03-08 09:23:18 +0700

    add luxon

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-console.log('Hello Docker 2');
+const luxon = require('luxon');
+const dt = luxon.DateTime.local();
+console.log('Hello Docker 2 '+ dt.toLocaleString());
 console.log('this is jenkins test');
