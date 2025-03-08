9e2fcf96b527aaa9226384a4e031722b9006f727
Enrico <enricoadi49@gmail.com>
2025-03-08 09:34:53 +0700

    add environments

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const luxon = require('luxon');
 const dt = luxon.DateTime.local();
+const param = process.argv[2];
 console.log('Hello Docker 2 '+ dt.toLocaleString());
-console.log('this is jenkins test');
+console.log('this is jenkins '+param);
